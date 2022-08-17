0074808e4262008f4790ef2bf1068e5a945ceaed
shanearcaro <smarcaro@gmail.com>
2022-08-17 15:31:41 -0400

    Adding custom colors: might not be used

--- app/src/main/java/com/shane/calculator/ui/theme/Color.kt
+++ app/src/main/java/com/shane/calculator/ui/theme/Color.kt
@@ -2,7 +2,17 @@ package com.shane.calculator.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+// Dark Theme colors
+val DarkBackground = Color(0xFF22252D)
+val DarkForeground = Color(0xFF292D36)
+val DarkText = Color(0xFFFFFFFF)
+
+// Light Theme colors
+val LightBackground = Color(0xFFFFFFFF)
+val LightForeground = Color(0xFFF9F9F9)
+val LightText = Color(0xFF292D36)
+
+// Calculator colors
+val ButtonRed = Color(0xFFEB6565)
+val ButtonGreen = Color(0xFF3CFBD8)
+
